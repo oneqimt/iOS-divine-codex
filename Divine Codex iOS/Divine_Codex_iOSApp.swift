4353862547e51657225d991ac03a74483116d66a
Dennis Miller <oneqimt@gmail.com>
2026-05-28 10:46:12 -0700

    Initial Commit

---
+++ Divine Codex iOS/Divine_Codex_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Divine_Codex_iOSApp.swift
+//  Divine Codex iOS
+//
+//  Created by Dennis Miller on 5/28/26.
+//
+
+import SwiftUI
+
+@main
+struct Divine_Codex_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
